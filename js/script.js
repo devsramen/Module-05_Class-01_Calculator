c2a905108e88f4630fabecd294b02cd4a6ead64d
Ramen Biswas <printrb72@gmail.com>
2023-12-28 17:57:43 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,65 @@
+var button = document.getElementsByTagName("button");
+var screenData = document.getElementById("screenData");
+
+function Plus(){
+    screenData.innerText = button[0].innerText;
+}
+function Minus(){
+    screenData.innerText = button[1].innerText;
+}
+function Multiplication(){
+    screenData.innerText = button[2].innerText;
+}
+function Divide(){
+    screenData.innerText = button[3].innerText;
+}
+function Nine(){
+    screenData.innerText = button[4].innerText;
+}
+function Eight(){
+    screenData.innerText = button[5].innerText;
+}
+function Seven(){
+    screenData.innerText = button[6].innerText;
+}
+function Six(){
+    screenData.innerText = button[7].innerText;
+}
+function Five(){
+    screenData.innerText = button[8].innerText;
+}
+function Four(){
+    screenData.innerText = button[9].innerText;
+}
+function Three(){
+    screenData.innerText = button[10].innerText;
+}
+function Tow(){
+    screenData.innerText = button[11].innerText;
+}
+function One(){
+    screenData.innerText = button[12].innerText;
+}
+function Zero(){
+    screenData.innerText = button[13].innerText;
+}
+function Dot(){
+    screenData.innerText = button[14].innerText;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(button[2]);
